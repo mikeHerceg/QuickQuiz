10e52a05aacfd88118aea2886ff9e97f56f45d2a
mikeHerceg <mikehercegdesigns@gmail.com>
2024-02-28 07:43:06 -0500

    building page container

---
+++ src/components/layout/pagecontainer.component.tsx
@@ -0,0 +1,51 @@
+import {
+  Box,
+  Heading,
+  HStack,
+  SkipNavContent,
+  SkipNavLink,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { FunctionComponent, PropsWithChildren } from "react";
+
+interface PageContainerProps extends PropsWithChildren {
+  title: string;
+  nav?: JSX.Element;
+}
+
+export const PageContainer: FunctionComponent<PageContainerProps> = ({
+  title,
+  nav,
+  children,
+}) => {
+  return (
+    <>
+      <SkipNavLink>Skip to content</SkipNavLink>
+      <VStack
+        padding={4}
+        as="main"
+        justifyContent={"center"}
+        gap={3}
+        width="100vw"
+        height="100vh"
+        maxW="1600px"
+        margin="auto"
+      >
+        <HStack as="header" justifyContent="space-between" width="100%">
+          <Text fontSize="2xl" fontWeight="bold">
+            QUICK QUIZ
+          </Text>
+          {nav}
+        </HStack>
+        <SkipNavContent />
+        <Heading as="h1" mb={6}>
+          {title}
+        </Heading>
+        <Box as="section" flexGrow={1} maxW="900px">
+          {children}
+        </Box>
+      </VStack>
+    </>
+  );
+};
--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,15 +1,24 @@
+import { HStack } from "@chakra-ui/react";
 import { FunctionComponent } from "react";
 import { Link } from "react-router-dom";
+import { PageContainer } from "../../components";
 import { AllQuizes } from "../../quizes/all.quiz";
 
 export const Home: FunctionComponent = () => {
   return (
-    <>
-      {AllQuizes.map((quiz) => (
-        <Link key={quiz.id} to={`/quiz/${quiz.id}`}>
-          {quiz.title}
-        </Link>
-      ))}
-    </>
+    <PageContainer
+      title="Home"
+      nav={
+        <HStack gap={3}>
+          {AllQuizes.map((quiz) => (
+            <Link key={quiz.id} to={`/quiz/${quiz.id}`}>
+              {quiz.title}
+            </Link>
+          ))}
+        </HStack>
+      }
+    >
+      hello
+    </PageContainer>
   );
 };
