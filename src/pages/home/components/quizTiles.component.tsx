f648b5f8e0d80f2765782fca3ec98497f8d8a122
mikeHerceg <mikehercegdesigns@gmail.com>
2024-03-21 07:06:56 -0400

    adding search function

--- src/pages/home/components/quizTiles.component.tsx
+++ src/pages/home/components/quizTiles.component.tsx
@@ -2,11 +2,41 @@ import { QuizSummary } from "../../../components/cards";
 import { AllQuizes } from "../../../quizes/all.quiz";
 import { Spacing } from "../../../theme/spacing.theme";
 import { Grid } from "@chakra-ui/react";
+import { useFilterContext } from "../filter.context";
+import { useEffect, useState } from "react";
 
 export const QuizTiles = () => {
+  const { filters } = useFilterContext();
+  const [quizzes, setQuizzes] = useState(AllQuizes);
+  useEffect(() => {
+    const { category, search } = filters;
+    // filter by category
+    const byCategory = category
+      ? AllQuizes.filter(({ category }) => category === category)
+      : AllQuizes;
+    // then by search
+    const bySearch = search
+      ? byCategory.filter(({ title, description }) => {
+          const searchValue = search.toLowerCase() ?? "";
+
+          if (title.toLowerCase().includes(searchValue)) {
+            return true;
+          }
+
+          if (description.toLowerCase().includes(searchValue)) {
+            return true;
+          }
+
+          return false;
+        })
+      : byCategory;
+
+    //set results
+    setQuizzes(bySearch);
+  }, [filters]);
   return (
     <Grid templateColumns="repeat(4, 1fr)" gap={Spacing.md}>
-      {AllQuizes.map(({ title, description, category, id }) => (
+      {quizzes.map(({ title, description, category, id }) => (
         <QuizSummary
           key={id}
           title={title}
--- src/pages/home/components/searchAndFilter.component.tsx
+++ src/pages/home/components/searchAndFilter.component.tsx
@@ -1,11 +1,31 @@
-import { Divider, HStack } from "@chakra-ui/react";
+import { Divider, HStack, Input, Select } from "@chakra-ui/react";
+import { ChangeEvent } from "react";
 import { Spacing } from "../../../theme";
+import { Category } from "../../../types/globals.types";
+import { useFilterContext } from "../filter.context";
 
 export const SearchAndFilter = () => {
+  const { filters, setFilters } = useFilterContext();
+
+  const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    setFilters({ ...filters, search: e.target.value });
+  };
+  const handleSelect = (e: ChangeEvent<HTMLSelectElement>) => {
+    console.log(e);
+    setFilters({ ...filters });
+  };
+
   return (
     <>
       <HStack gap={Spacing.md}>
-        <span>Search Input</span> <span>category picker</span>
+        <Input
+          onChange={handleSearch}
+          value={filters?.search ?? ""}
+          name={"search"}
+        />
+        <Select value={filters?.category ?? ""} onChange={handleSelect}>
+          <option value={Category.History}>{Category.History}</option>
+        </Select>
       </HStack>
       <Divider my={Spacing.lr} />
     </>
